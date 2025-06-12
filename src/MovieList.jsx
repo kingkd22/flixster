b7fa1a845871c28f573e5fe7b5e056db695a8591
Kingston Davies <kingstond22@meta.com>
2025-06-11 22:33:59 -0700

    sort bug fixe -> cards dont duplicate anymore; Modal css adjusted to fit better on the page

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -5,17 +5,21 @@ import "./MovieList.css"
 
 function MovieList() {
 
-    let [movies, setMovies] = useState([])
-    let [page, setPageNumber] = useState(1)
-    const apiKey = import.meta.env.VITE_APP_API_KEY;
+    const [movies, setMovies] = useState([])
+    const [page, setPageNumber] = useState(1)
+    const [sortedMovies, setSortedMovies] = useState([])
+    const [sortOption, setSortOption] = useState("")
     const [searchQuery, setSearchQuery] = useState('')
 
     
-    function fetchFunction() {
+    const apiKey = import.meta.env.VITE_APP_API_KEY;
+
+    
+    function fetchFunction(customPage = page) {
 
         const url = searchQuery
-            ? `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&page=${page}`
-            : `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`;
+            ? `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&page=${customPage}`
+            : `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${customPage}`;
         
         const options = {
             method: 'GET',
@@ -28,8 +32,17 @@ function MovieList() {
         fetch(url, options)
             .then(res => res.json())
             .then(data => {
-                setMovies(prevMovies => (page > 1 ? [...prevMovies, ...data.results] : data.results));
-                console.log(movies)
+                setMovies(prevMovies => {
+                    const combined = customPage > 1 
+                        ? [...prevMovies, ...data.results] 
+                        : data.results;
+                    
+                    const uniqueMovies = Array.from(
+                        new Map(combined.map(movie => [movie.id, movie])).values()
+                    );
+                    return uniqueMovies;
+                    
+                });
             })
             .catch(err => console.error(err));
 
@@ -39,6 +52,10 @@ function MovieList() {
         fetchFunction()
     }, [page, searchQuery]);
 
+    useEffect(() => {
+        sortMovies(movies, sortOption)
+    }, [movies, sortOption])
+
     function handleSearch(query) {
         setSearchQuery(query);
         setPageNumber(1)
@@ -46,16 +63,26 @@ function MovieList() {
 
     function loadMore() {
         const nextPage = page + 1;
-        setPageNumber(nextPage)
-        fetchFunction();
+        setPageNumber(nextPage);
     }
 
     function resetPage() {
-        const homePage = 1;
-        setPageNumber(homePage);
-        const cleared = '';
-        setSearchQuery(cleared)
-        fetchFunction();
+        setPageNumber(1);
+        setSearchQuery('');
+    }
+
+    function sortMovies(movieList, option) {
+        let sorted = [...movies];
+
+        if (option === "title") {
+            sorted.sort((a, b) => a.title.localeCompare(b.title))
+        } else if (option === "release_date") {
+            sorted.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+        } else if (option === "vote_average") {
+            sorted.sort((a, b) => b.vote_average - a.vote_average);
+        }
+
+        setSortedMovies(sorted);
     }
 
     return (
@@ -63,11 +90,19 @@ function MovieList() {
             <header>
                 <button onClick={resetPage} className="resetButton">Home</button>
                 <SearchForm onSearch={handleSearch} />
+                <div className="sort">
+                    <select onChange={(e) => setSortOption(e.target.value)}>
+                        <option value="">Sort By...</option>
+                        <option value="title">Title (A-Z)</option>
+                        <option value="release_date">Release Date (Newest to Oldest)</option>
+                        <option value="vote_average">Vote Average (High to Low)</option>
+                    </select>
+                </div>
             </header>
 
             <main className="grid">
                 <div className="grid">
-                    {movies.map((movie) => (
+                    {sortedMovies.map((movie) => (
                         <MovieCard
                             key={movie.id}
                             id={movie.id}
